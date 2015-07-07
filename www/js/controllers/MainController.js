54426ae2d5ce5d3fed91f317f4a00a4cead42408
Yuan Ma <405347545@qq.com>
2015-07-07 19:42:37 -0400

    1
    1

---
+++ www/js/controllers/MainController.js
@@ -0,0 +1,12 @@
+app.controller('MainController',['$scope','bus',function($scope,bus){
+	//bus.success(function(info){
+
+		
+	//	$scope.busInfo = info;
+	bus.success(function(info){
+
+		
+		$scope.busInfo = info;
+	})
+}]
+);
